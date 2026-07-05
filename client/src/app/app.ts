4c9decec62a1983354fce6c140aa787189bb8f8d
vipgarg <gargvipin.97@gmail.com>
2026-07-06 00:28:29 +0530

    end of section3

---
+++ client/src/app/app.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = 'Dating app';
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+    // this.http.get('https://localhost:5001/api/members').subscribe({
+    //   next: response => this.members.set(response),
+    //   error: error=> console.log(error),
+    //   complete: () => console.log('completed the http request')
+    // })
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
