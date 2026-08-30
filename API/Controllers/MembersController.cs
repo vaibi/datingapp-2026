154e0aba2d5df92bbb23ef6a28278978cd2e2d41
vipgarg <gargvipin.97@gmail.com>
2026-08-30 23:46:45 +0530

    End of section 11

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Authorize]
-    public class MembersController(IMemberRepository memberRepository) : BaseAPIController
+    public class MembersController(IMemberRepository memberRepository, IPhotoService photoService) : BaseAPIController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
@@ -56,8 +56,88 @@ namespace API.Controllers
 
             return BadRequest("Failed to update member");
         }
-    }
+    
 
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<Photo>> AddPhoto([FromForm] IFormFile file)
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if(member == null) return BadRequest("Cannot update member");
+
+            var result = await photoService.UploadPhotoAsync(file);
+
+            if(result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+                MemberID = User.GetMemberId()
+            };
+
+            if(member.ImageUrl == null)
+            {
+                member.ImageUrl = photo.Url;
+                member.User.ImageUrl = photo.Url;
+            }
+
+            member.Photos.Add(photo);
+
+            if(await memberRepository.SaveAllAsync()) return photo;
+
+            return BadRequest("Problem adding photo");
+        }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if(member == null) return BadRequest("Cannot get member from token");
+
+            var photo = member.Photos.SingleOrDefault(x => x.Id == photoId);
+
+            if(member.ImageUrl == photo?.Url || photo == null)
+            {
+                return BadRequest("Cannot set this as main image");
+            }
+
+            member.ImageUrl = photo.Url;
+            member.User.ImageUrl = photo.Url;
+
+            if(await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Problem settinh main photo");
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+             var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if(member == null) return BadRequest("Cannot get member from token");
+
+            var photo = member.Photos.SingleOrDefault(x => x.Id == photoId);
+
+            if(photo == null || member.ImageUrl == photo.Url)
+            {
+                return BadRequest("This photo cannot be deleted");
+            }
+
+            if(photo.PublicId != null)
+            {
+                var result = await photoService.DeletePhotoAsync(photo.PublicId);
+                if(result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            member.Photos.Remove(photo);
+
+            if(await memberRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the photo");
+        }
+    }
 }
 
 
---
+++ client/src/shared/star-button/star-button.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-star-button',
+  imports: [],
+  templateUrl: './star-button.html',
+  styleUrl: './star-button.css',
+})
+export class StarButton {
+  disabled = input<boolean>(false);
+  selected  = input<boolean>(false);
+  clickEvent = output<Event>();
+  
+  onClick(event: Event)
+  {
+    this.clickEvent.emit(event);
+  }
+}
